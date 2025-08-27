67079559d82b911f46fa648dfafae55c4a44744a
Joan Nassr Conte <joangithub2007@gmail.com>
2025-08-27 08:59:50 -0300

    update

--- aplicacao/aplicacao.java
+++ aplicacao/aplicacao.java
@@ -13,60 +13,80 @@ public class aplicacao {
         public static void main(String[] args) {
             Scanner ler = new Scanner(System.in);
             
+            while (true) {
+
             System.out.println("\nOlá, seja bem-vindo!:");
             System.out.println("\nEscolha qual das geometrias deseja calcular:");
 
     
-            System.out.printf("Espacial (1) / Plana (2) ");
+            System.out.printf("Espacial (1) / Plana (2) / Sair (0) ");
             escolhaConta = ler.nextInt();
 
-            if (1 == escolhaConta) {
+            if (escolhaConta == 0) {
+                System.out.println("Programa encerrado. Até mais!");
+                break; 
+            }
+
+            else if (1 == escolhaConta) {
                 System.out.println("\nVocê escolheu a Geometria Espacial.");
             
-                System.out.printf("Qual figura você deseja calcular: Pirâmide (1) / Esfera (2) / Cones (3) / Troncos (4) / Cilindro (5) / Prismas (6) ");
+                System.out.printf("Qual figura você deseja calcular: Pirâmide (1) / Esfera (2) / Cones (3) / Troncos (4) / Cilindro (5) / Prismas (6) / Sair (0) ");
                 escolhaFigura = ler.nextInt();
-            
-                if ( 1 == escolhaFigura) {
-                    System.out.printf("Qual cálculo de Pirâmide você deseja: Área Lateral (1) / Área Total (2) / Volume (3) ");
+
+        
+                if (0 == escolhaFormula) {
+                    System.out.println("Programa encerrado. Até mais!");
+                    break; 
+                }
+
+                else if ( 1 == escolhaFigura) {
+                    System.out.printf("Qual cálculo de Pirâmide você deseja: Área Lateral (1) / Área Total (2) / Volume (3) / Sair (0) ");
                     escolhaFormula = ler.nextInt();
+
+                    
 
                     if (escolhaFormula == 1) { 
                         
                         System.out.print("Digite o perímetro da base: "); double pBase = ler.nextDouble(); 
-                    System.out.print("Digite o apótema: "); double apotema = ler.nextDouble(); 
-                    double resultado = espacial.areaLateralPiramide (pBase, apotema); 
-                    System.out.println("O resultado da Área Lateral é " + resultado); }
+                        System.out.print("Digite o apótema: "); double apotema = ler.nextDouble(); 
+                        double resultado = espacial.areaLateralPiramide (pBase, apotema); 
+                        System.out.println("O resultado da Área Lateral é " + resultado); }
 
                     else if (2 == escolhaFormula) {
 
                         System.out.print("Digite a Área da Base: "); double areaBase = ler.nextDouble(); 
-                    System.out.print("Digite a Área Lateral: "); double areaLateral = ler.nextDouble(); 
-                    double resultado = espacial.areaTotalPiramide (areaBase, areaLateral); 
-                    System.out.println("O resultado da Área Total é " + resultado);
+                        System.out.print("Digite a Área Lateral: "); double areaLateral = ler.nextDouble(); 
+                        double resultado = espacial.areaTotalPiramide (areaBase, areaLateral); 
+                        System.out.println("O resultado da Área Total é " + resultado);
                         
                     }
 
                     else if (3 == escolhaFormula) {
 
                         System.out.print("Digite a Área da Base: "); double areaBase = ler.nextDouble(); 
-                    System.out.print("Digite a Altura: "); double altura = ler.nextDouble(); 
-                    double resultado = espacial.volumePiramide (areaBase, altura); 
-                    System.out.println("O resultado do Volume é " + resultado);
+                        System.out.print("Digite a Altura: "); double altura = ler.nextDouble(); 
+                        double resultado = espacial.volumePiramide (areaBase, altura); 
+                        System.out.println("O resultado do Volume é " + resultado);
 
 
+                    }
+
+                    else if (0 == escolhaFormula) {
+                        System.out.println("Programa encerrado. Até mais!");
+                        break; 
                     }
                 }
             
                 else if (2 == escolhaFigura) {
-                    System.out.printf("Qual cálculo de Esfera você deseja: Área Superficial (1) / Volume (2) ");
+                    System.out.printf("Qual cálculo de Esfera você deseja: Área Superficial (1) / Volume (2) / Sair (0) ");
                     escolhaFormula = ler.nextInt();
             
                     if (1 == escolhaFormula) {
 
 
                         System.out.print("Digite o Raio: "); double raio = ler.nextDouble(); 
-                    double resultado = espacial.areaSuperficial (raio); 
-                    System.out.println("O resultado da Área Superficial é " + resultado);
+                        double resultado = espacial.areaSuperficial (raio); 
+                        System.out.println("O resultado da Área Superficial é " + resultado);
 
                     }
 
@@ -76,80 +96,142 @@ public class aplicacao {
                     else if (2 == escolhaFormula) {
 
                         System.out.print("Digite o Raio: "); double raio = ler.nextDouble(); 
-                    double resultado = espacial.volumeEsfera(raio); 
-                    System.out.println("O resultado do Volume é " + resultado);
+                        double resultado = espacial.volumeEsfera(raio); 
+                        System.out.println("O resultado do Volume é " + resultado);
 
+                    }
+
+                    else if (0 == escolhaFormula) {
+                        System.out.println("Programa encerrado. Até mais!");
+                        break; 
                     }
                 }
             
                 else if (3 == escolhaFigura) {
-                    System.out.printf("Qual cálculo de Cones você deseja: Área Lateral (1) / Área da Base (2) / Volume (3) ");
+                    System.out.printf("Qual cálculo de Cones você deseja: Área Lateral (1) / Área da Base (2) / Volume (3) / Sair (0) ");
                     escolhaFormula = ler.nextInt();
 
                     if (1 == escolhaFormula) {
-                        System.out.printf("Vamos calcular a Área Lateral do cone! Insira o valor ... : ");
-                        valores = ler.nextInt();
+
+                        System.out.print("Digite o Raio: "); double raio = ler.nextDouble(); 
+                        System.out.print("Digite a Geratriz: "); double geratriz = ler.nextDouble(); 
+                        double resultado = espacial.areaLateralCone(geratriz, raio); 
+                        System.out.println("O resultado da Área Lateral é " + resultado);
+
                     }
 
                     else if (2 == escolhaFormula) {
-                        System.out.printf("Vamos calcular a Área da Base do cone! Insira o valor ... : ");
-                        valores = ler.nextInt();
+
+                        System.out.print("Digite o Raio: "); double raio = ler.nextDouble(); 
+                        double resultado = espacial.areaBaseCone(raio); 
+                        System.out.println("O resultado da Área da Base é " + resultado);
+
                     }
 
                     else if (3 == escolhaFormula) {
-                        System.out.printf("Vamos calcular o Volume do cone! Insira o valor ... : ");
-                        valores = ler.nextInt();
+
+                        System.out.print("Digite o Raio: "); double raio = ler.nextDouble(); 
+                        System.out.print("Digite a Altura: "); double altura = ler.nextDouble(); 
+                        double resultado = espacial.volumeCone(altura, raio); 
+                        System.out.println("O resultado do Volume é " + resultado);
+
+                    }
+
+                    else if (0 == escolhaFormula) {
+                        System.out.println("Programa encerrado. Até mais!");
+                        break; 
                     }
                 }
             
                 else if (4 == escolhaFigura) {
-                    System.out.printf("Qual cálculo de Troncos você deseja: Área Lateral (1) / Área Total (2) / Volume (3) ");
+                    System.out.printf("Qual cálculo de Troncos você deseja: Volume (1) / Sair (0) ");
                     escolhaFormula = ler.nextInt();
 
                     if (1 == escolhaFormula) {
-                        System.out.printf("Vamos calcular a Área Superficial! Insira o valor do raio: ");
-                        valores = ler.nextInt();
+
+                        System.out.print("Digite o Raio menor: "); double raioMenor = ler.nextDouble(); 
+                        System.out.print("Digite o Raio maior: "); double raioMaior = ler.nextDouble(); 
+                        System.out.print("Digite a Altura: "); double altura = ler.nextDouble(); 
+                        double resultado = espacial.volumeTronco(raioMenor, altura, raioMaior); 
+                        System.out.println("O resultado do Volume é " + resultado);
+                    }
+
+                    else if (0 == escolhaFormula) {
+                        System.out.println("Programa encerrado. Até mais!");
+                        break; 
                     }
                 }
             
                 else if (5 == escolhaFigura) {
-                    System.out.printf("Qual cálculo de Cilindro você deseja: Área da Base (1) / Área Lateral (2) / Volume (3) ");
+                    System.out.printf("Qual cálculo de Cilindro você deseja: Área da Base (1) / Área Lateral (2) / Volume (3) / Sair (0) ");
                     escolhaFormula = ler.nextInt();
 
                     if (1 == escolhaFormula) {
-                        System.out.printf("Vamos calcular a Área da Base do cilindro! Insira o valor ... : ");
-                        valores = ler.nextInt();
+
+                        System.out.print("Digite o Raio: "); double raio = ler.nextDouble(); 
+                        double resultado = espacial.areaBaseCilindro(raio); 
+                        System.out.println("O resultado da Área da Base é " + resultado);
                     }
 
                     else if (2 == escolhaFormula) {
-                        System.out.printf("Vamos calcular a Área Lateral do cilindro! Insira o valor ... : ");
-                        valores = ler.nextInt();
+
+                        System.out.print("Digite o Raio: "); double raio = ler.nextDouble();
+                        System.out.print("Digite a Altura: "); double altura = ler.nextDouble();  
+                        double resultado = espacial.areaLateralCilindro(raio, altura); 
+                        System.out.println("O resultado da Área Lateral é " + resultado);
                     }
 
                     else if (3 == escolhaFormula) {
-                        System.out.printf("Vamos calcular o Volume do cilindro! Insira o valor ... : ");
-                        valores = ler.nextInt();
+
+                        System.out.print("Digite o Raio: "); double raio = ler.nextDouble();
+                        System.out.print("Digite a Altura: "); double altura = ler.nextDouble();  
+                        double resultado = espacial.volumeCilindro(raio, altura); 
+                        System.out.println("O resultado do Volume é " + resultado);
+                    }
+
+                    else if (0 == escolhaFormula) {
+                        System.out.println("Programa encerrado. Até mais!");
+                        break; 
                     }
                 }
             
                 else if (6 == escolhaFigura) {
-                    System.out.printf("Qual cálculo de Prisma você deseja: Área Lateral (1) / Área Total (2) / Volume (3) ");
+                    System.out.printf("Qual cálculo de Prisma você deseja: Área Lateral (1) / Área Total (2) / Volume (3) / Sair (0) ");
                     escolhaFormula = ler.nextInt();
 
                     if (1 == escolhaFormula) {
-                        System.out.printf("Vamos calcular a Área Lateral do Prisma! Insira o valor ... : ");
-                        valores = ler.nextInt();
+
+                        System.out.print("Digite a Área de faces: "); double areaFaces = ler.nextDouble();
+                        System.out.print("Digite a Altura: "); double altura = ler.nextDouble();
+                        System.out.print("Digite a Altura: "); double numFaces = ler.nextDouble();  
+                        double resultado = espacial.areaLateralPrisma(areaFaces, altura, numFaces); 
+                        System.out.println("O resultado da Área Lateral é " + resultado);
                     }
 
                     else if (2 == escolhaFormula) {
-                        System.out.printf("Vamos calcular a Área Total do Prisma! Insira o valor ... : ");
-                        valores = ler.nextInt();
+
+                        System.out.print("Digite a Área da Base: "); double areaBase = ler.nextDouble();
+                        System.out.print("Digite a Área Lateral: "); double areaLateral = ler.nextDouble();  
+                        double resultado = espacial.areaTotalPrisma(areaBase, areaLateral); 
+                        System.out.println("O resultado da Área Total é " + resultado);
                     }
 
                     else if (3 == escolhaFormula) {
-                        System.out.printf("Vamos calcular o Volume do Prisma! Insira o valor ... : ");
-                        valores = ler.nextInt();
+
+                        System.out.print("Digite a Área da Base: "); double areaBase = ler.nextDouble();
+                        System.out.print("Digite a Altura: "); double altura = ler.nextDouble();  
+                        double resultado = espacial.volumePrisma(areaBase, altura); 
+                        System.out.println("O resultado do Volume é " + resultado);
+
+
                     }
+
+                    else if (0 == escolhaFormula) {
+                        System.out.println("Programa encerrado. Até mais!");
+                        break; 
+                    }
+
+
 
                     
                 }
@@ -159,10 +241,18 @@ public class aplicacao {
                 System.out.println("Você escolheu a Geometria Plana.");
             }
 
+            else if (3 >= escolhaConta) {
+                System.out.println("Escolha inválida.");
+                break;
+            }
 
-        System.out.println(escolhaConta);
+
+
 
 }
+
 
     
-}
+        }
+
+    }
--- geo/espacial.java
+++ geo/espacial.java
@@ -25,20 +25,20 @@ public class espacial extends geometria {
         }
     
     
-        public static double areaBaseCilindro(double raio, double pi) {
-            return pi * Math.pow(raio, 2); 
+        public static double areaBaseCilindro(double raio) {
+            return 3.14 * Math.pow(raio, 2); 
     
         }
     
     
-        public static double areaLateralCilindro(double raio, double pi, double altura) {
-            return 2 * pi * raio * altura;
+        public static double areaLateralCilindro(double raio, double altura) {
+            return 2 * 3.14 * raio * altura;
     
     
         }
     
-        public static double volumeCilindro(double raio, double pi, double altura) {
-            return pi * Math.pow(raio, 2) * altura;
+        public static double volumeCilindro(double raio, double altura) {
+            return 3.14 * Math.pow(raio, 2) * altura;
     
         }
     
@@ -59,24 +59,24 @@ public class espacial extends geometria {
             
         }
     
-        public static double areaLateralCone (double pi, double raio, double geratriz) {
-            return pi * raio * geratriz;
+        public static double areaLateralCone (double raio, double geratriz) {
+            return 3.14 * raio * geratriz;
     
         }
     
-        public static double areaBaseCone (double pi, double raio) {
-            return pi * Math.pow(raio, 2);
+        public static double areaBaseCone (double raio) {
+            return 3.14 * Math.pow(raio, 2);
     
         }
     
-        public static double volumeCone (double pi, double raio, double altura) {
-            return pi * Math.pow(raio, 2) * altura / 3;
+        public static double volumeCone ( double raio, double altura) {
+            return 3.14 * Math.pow(raio, 2) * altura / 3;
     
         }
     
     
-        public static double volumeTronco (double raioMenor, double raioMaior, double pi, double altura) {
-            return pi * altura * ( Math.pow(raioMenor, 2) + raioMenor * raioMaior + Math.pow(raioMaior, 2) ) / 3;
+        public static double volumeTronco (double raioMenor, double raioMaior, double altura) {
+            return 3.14 * altura * ( Math.pow(raioMenor, 2) + raioMenor * raioMaior + Math.pow(raioMaior, 2) ) / 3;
     
         }
     
